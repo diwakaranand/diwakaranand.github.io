3f489fd36b20b174f92e306d9de0ed2857258bbb
diwakaranand <diwakaranand40@gmail.com>
2020-12-19 21:42:54 +0530

    first commit

---
+++ MyClass.java
@@ -0,0 +1,6 @@
+public class MyClass {
+	  public static void main(String[] args) {
+		  String name="DIWAKAR";
+	    System.out.println(name);
+	  }
+}
---
+++ hello.py
@@ -0,0 +1,26 @@
+print('welcome')
+print(2*4)
+print("python are easy to use")
+print("hello"+" world")
+gretting='diwakar'
+name='anand'
+print(gretting+name)
+print(gretting+' '+name)
+print('dipak')
+var=2
+print(var)
+print(gretting +' '+name+'how are you ')
+msg='{},{}.howare you?'.format(gretting,name)
+print(msg)
+msg1=f'{gretting},{name}.how are you?'
+mesg='Knowledge Self'
+print(mesg)
+print(type(mesg))
+print(mesg.lower())
+print(mesg.upper())
+
+print(len(mesg))
+print(mesg.find('Self'))
+print(mesg[10])
+
+
